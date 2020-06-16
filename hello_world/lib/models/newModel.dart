5344ce5aaf520dcd64dc5b25d6019c75a4ad516d
Uladzimir Paliukhovich <Uladzimir_Paliukhovich@epam.com>
2020-06-16 12:01:41 +0300

    removed not needed files

--- hello_world/lib/models/newModel.dart
+++
@@ -1,9 +0,0 @@
-class NewModel{
-    String body;
-    String head;
-    String id;
-
-    NewModel(this.body, this.head, this.id);
-
-    NewModel.fromJson(Map<String, dynamic> json) : body = json["body"], head = json["head"], id = json["id"];    
-}
